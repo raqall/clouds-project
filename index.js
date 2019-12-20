024ee58a7e7d5054712ca919496054241237995d
raqall <raqall@outlook.com>
2019-12-21 00:28:29 +0100

    neo4j working

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 let dbUsername = 'cloudprojectuser'
 let dbPassword = 'b.9jCoZaxEsKca.UdvPlipZHiKBljDS'
-let dbConnectionString = "bolt://hobby-blfmhkkeanodgbkepkofbfdl.dbs.graphenedb.com:24787"
+let dbUrl = "hobby-blfmhkkeanodgbkepkofbfdl.dbs.graphenedb.com:24780/db/data/"
 
-var neo4j = require('neo4j-driver');
-var driver = neo4j.driver(dbConnectionString, neo4j.auth.basic(dbUsername, dbPassword));
+var neo4j = require('neo4j');
+var db = new neo4j.GraphDatabase("https://" + dbUsername + ":" + dbPassword + "@" + dbUrl);
 
-var session = driver.session();
 
 const express = require('express')
 const path = require('path')
@@ -29,28 +28,73 @@ express()
   
   .get('/', (req, res) => res.render('pages/index'))
 
-  .get('/list', (req, res) => res.render('pages/list'))
-  .get('/list/create', (req, res) => res.render('pages/create'))
-
-  .get('/graph', (req, res) => res.render('pages/graph'))
-
-
-  .post('/list/create', (req, res) => {
-    
-    session
-        .run('CREATE (n:Person {name:"Bob"})')
-        .then(function(result) {
-            console.log(result)
-            session.close();
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
-
-    res.redirect('/list')
-    
+  .get('/list', (req, res) => {
+    db.cypher({
+        query: 'MATCH (n:Person) return ID(n) AS id, n.name AS name, n.surname AS surname'
+      }, function(err, results){
+        if(err){
+          res.render('pages/error')
+        } else {
+          res.render('pages/list', { people: results })
+        }      
+    })
   })
 
+  .get('/list/create', (req, res) => {
+    db.cypher({
+      query: 'MATCH (n:Person) return ID(n) AS id, n.name AS name, n.surname AS surname'
+      }, function(err, results){
+        if(err){
+          res.render('pages/error')
+        } else {
+          res.render('pages/create', { people: results })
+        }      
+    })
+  })
+
+  .post('/list/create', (req, res) => {
+    db.cypher({
+        query: 'CREATE (:Person {name:{paramName}, surname:{paramSurname}})',
+        params: {
+          paramName: req.body.name,
+          paramSurname: req.body.surname
+        }
+      }, function(err, results){
+        if(err){
+          res.render('pages/error')
+        } else {
+          res.redirect('/list')
+        }      
+    })
+  })
+
+  .get('/list/update/:id', (req, res) => {
+    db.cypher({
+      query: 'MATCH (n:Person) WHERE ID(n) = 3 return ID(n) AS id, n.name AS name, n.surname AS surname'
+      }, function(err, results){
+        if(err){
+          res.render('pages/error')
+        } else {
+          console.log(results)
+          res.render('pages/create', { person: results[0], edit: true })
+        }      
+    })
+  })
+
+  .get('/graph', (req, res) => {
+    
+    db.cypher({
+        query: 'MATCH (n:Person) return ID(n) AS id'
+      }, function(err, results){
+        if(err){
+          res.render('pages/error')
+        } else {
+          console.log(results)
+          res.render('pages/create', { people: results })
+        }      
+    })
+
+  })
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
