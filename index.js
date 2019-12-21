168cf5eaf125bcdf2dcc807a2ed37b8edede7eeb
raqall <raqall@outlook.com>
2019-12-21 13:59:25 +0100

    Create, Read, Delete

--- index.js
+++ index.js
@@ -14,95 +14,142 @@ const PORT = process.env.PORT || 5000
     Handlers for functionalities
 */
 
-function createPerson(){
-  console.log('addig person')
+function createPerson() {
+   console.log('addig person')
 }
 
 express()
-  .use(express.urlencoded({ extended: true }))
+   .use(express.urlencoded({
+      extended: true
+   }))
 
-  .use(express.static(path.join(__dirname, 'public')))
+   .use(express.static(path.join(__dirname, 'public')))
 
-  .set('view engine', 'pug')
-  .set('views', path.join(__dirname, 'views'))
-  
-  .get('/', (req, res) => res.render('pages/index'))
+   .set('view engine', 'pug')
+   .set('views', path.join(__dirname, 'views'))
 
-  .get('/list', (req, res) => {
-    db.cypher({
-        query: 'MATCH (n:Person) return ID(n) AS id, n.name AS name, n.surname AS surname'
-      }, function(err, results){
-        if(err){
-          res.render('pages/error')
-        } else {
-          res.render('pages/list', { people: results })
-        }      
-    })
-  })
+   .get('/', (req, res) => res.render('pages/index'))
 
-  .get('/list/create', (req, res) => {
-    db.cypher({
-      query: 'MATCH (n:Person) return ID(n) AS id, n.name AS name, n.surname AS surname'
-      }, function(err, results){
-        if(err){
-          res.render('pages/error')
-        } else {
-          res.render('pages/create', { people: results })
-        }      
-    })
-  })
+   .get('/list', (req, res) => {
+      db.cypher({
+         query: 'MATCH (n:Person) return ID(n) AS id, n.name AS name, n.surname AS surname ORDER BY n.id'
+      }, function (err, results) {
+         if (err) {
+            res.render('pages/error')
+         } else {
+            res.render('pages/list', {
+               people: results
+            })
+         }
+      })
+   })
 
-  .post('/list/create', (req, res) => {
-    db.cypher({
-        query: 'CREATE (:Person {name:{paramName}, surname:{paramSurname}})',
-        params: {
-          paramName: req.body.name,
-          paramSurname: req.body.surname
-        }
-      }, function(err, results){
-        if(err){
-          res.render('pages/error')
-        } else {
-          res.redirect('/list')
-        }      
-    })
-  })
+   .get('/list/create', (req, res) => {
+      db.cypher({
+         query: 'MATCH (n:Person) return ID(n) AS id, n.name AS name, n.surname AS surname'
+      }, function (err, results) {
+         if (err) {
+            res.render('pages/error')
+         } else {
+            res.render('pages/create', {
+               people: results
+            })
+         }
+      })
+   })
 
-  .get('/list/update/:id', (req, res) => {
-    db.cypher({
-      query: 'MATCH (n:Person) WHERE ID(n) = {paramId} return ID(n) AS id, n.name AS name, n.surname AS surname',
-      params: {
-        paramId: req.body.id
-      }
-      }, function(err, results){
-        if(err){
-          res.render('pages/error')
-        } else {
-          console.log(results)
-          res.render('pages/create', { person: results[0], edit: true })
-        }      
-    })
-  })
+   .post('/list/create', (req, res) => {
+      db.cypher({
+         query: 'CREATE (:Person {name:{paramName}, surname:{paramSurname}})',
+         params: {
+            paramName: req.body.name,
+            paramSurname: req.body.surname
+         }
+      }, function (err, results) {
+         if (err) {
+            res.render('pages/error')
+         } else {
+            res.redirect('/list')
+         }
+      })
+   })
 
-  .post('/list/delete/:id', (req, res) => {
-    console.log("deleting: " + req.body.id)
-  })
+   .get('/list/update/:id', (req, res) => {
+      let query = 'MATCH (n) WHERE ID(n) = ' + req.params.id + ' RETURN ID(n) AS id, n.name AS name, n.surname AS surname'
+      db.cypher({
+         query: query
+      }, function (err, results) {
+         if (err) {
+            res.render('pages/error')
+         } else {
+            let person = results[0];
+            let query = 'MATCH (n:Person) return ID(n) AS id, n.name AS name, n.surname AS surname ORDER BY n.id'
+            db.cypher({
+               query: query
+            }, function (err, results) {
+               if (err) {
+                  res.render('pages/error')
+               } else {
+                  let people = results;
+                  res.render('pages/create', {
+                     person: person,
+                     people: people,
+                     edit: true
+                  })
+               }
+            })
+         }
+      })
+   })
 
-  .get('/graph', (req, res) => {
-    
-    db.cypher({
-        query: 'MATCH (n:Person) return ID(n) AS id'
-      }, function(err, results){
-        if(err){
-          res.render('pages/error')
-        } else {
-          console.log(results)
-          res.render('pages/create', { people: results })
-        }      
-    })
+   .post('/list/update/:id', (req, res) => {
+      console.log('update: ' + req.params.id)
+      // let query = 'MATCH (n) WHERE ID(n) = ' + req.params.id + ' RETURN n.name AS name, n.surname AS surname'
 
-  })
+      // db.cypher({
+      //    query: query
+      // }, function (err, results) {
+      //    if (err) {
+      //       res.render('pages/error')
+      //    } else {
+      //       let people = results;
+      //       res.render('pages/create', {
+      //          person: person,
+      //          people: people,
+      //          edit: true
+      //       })
+      //    }
+      // })
+   })
 
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+   .post('/list/delete/:id', (req, res) => {
+      let query = 'MATCH (n) WHERE ID(n) = ' + req.params.id + ' DETACH DELETE n'
+      db.cypher({
+         query: query
+      }, function (err, results) {
+         if (err) {
+            res.render('pages/error')
+         } else {
+            res.redirect('/list')
+         }
+      })
+   })
 
+   .get('/graph', (req, res) => {
 
+      db.cypher({
+         query: 'MATCH (n:Person) return ID(n) AS id'
+      }, function (err, results) {
+         if (err) {
+            res.render('pages/error')
+         } else {
+            console.log(results)
+            res.render('pages/create', {
+               people: results
+            })
+         }
+      })
+
+   })
+
+   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
